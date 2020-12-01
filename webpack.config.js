1f66985d6e0aa6b31cacf7bdb787d64308b20b0a
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-12-01 13:56:42 -0500

    Configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client'),
+    filename: './build/bundle.js'
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './client'),
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+};
