59f7c5f2de2df10f360961e40a256e3872304c2e
Mark Fusco <32991975+QuestionMark97@users.noreply.github.com>
2020-12-03 19:29:58 -0500

    Hid development database URL

--- server/model/model.js
+++ server/model/model.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const userSchema = require('./schema/userSchema');
 const cardSchema = require('./schema/cardSchema');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://flash-card-user:flash-card-user@cluster0.qmqwd.mongodb.net/flash-card-db?retryWrites=true&w=majority';
+if (process.env.NODE_ENV !== 'production') dotenv.config();
+
+const { MONGO_URI } = process.env;
 const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'flash-card-db' };
 
-mongoose.connect(MONGO_URI, OPTIONS)
-  .then(() => console.log('Connected to Mongo DB'))
-  .catch((err) => console.log(err));
+// eslint-disable-next-line no-console
+mongoose.connect(MONGO_URI, OPTIONS).catch((err) => console.log(err));
 
 const User = mongoose.model('user', userSchema);
 const Card = mongoose.model('card', cardSchema);
